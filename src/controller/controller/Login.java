8d602921b78554dd02f225c4a2f9e2840e829714
Denis Niwemugisha <dniwemugisha@go.rwanda.cmu.local>
2017-10-11 11:37:27 +0100

    servlet1 done: called DataLoad

---
+++ src/controller/controller/Login.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+import DatabaseConnection.DatabaseConnection;
+import model.PasswordHash;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	/**
+     * @see HttpServlet#HttpServlet()
+     */
+	
+	public Login(){
+		super();
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		// Retrieve username and password from the login request
+		HttpSession session= request.getSession(true);
+		boolean pwMatches =false;
+		String username = request.getParameter("Username");
+		String password = request.getParameter("Password");
+		// Checking if username and password aren't null
+		if (!username.equalsIgnoreCase("") || !password.equalsIgnoreCase("")) {
+			DatabaseConnection db = new DatabaseConnection();
+			//PasswordHash pH = new PasswordHash();
+			String hashedPassword = PasswordHash.getPasswordHash(password);
+			
+			pwMatches = db.login(username, hashedPassword);
+			//System.out.println(hashedPassword);
+			System.out.println(pwMatches);
+			
+			// Check user credentials entered
+			if(pwMatches){
+				response.sendRedirect("/practicum_project/DataLoad");
+			}
+			else{
+				response.sendRedirect("/practicum_project/index.jsp");
+			}
+		}
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request,response);
+	}
+}
